e3e2e502162c7c256bf0e811a440b24b19fa0215
Aristides Gonzalez <ariguille.gp@gmail.com>
2023-03-28 18:16:41 -0400

    initial npm build

--- js/build.go
+++ js/build.go
@@ -6,6 +6,29 @@ import (
 )
 
 func (p *Pipeline) Build(ctx context.Context, baseNodeImg string) error {
-	fmt.Println("Build Job")
+	src := p.Client.Host().Directory(".") // get the project source directory
+
+	npm := p.Client.Container().From("node"). // initialize new container from npm image
+							WithMountedDirectory("/src", src).WithWorkdir("/src"). // mount source directory to /src
+							WithExec([]string{"npm", "install"}).                  // execute npm install
+							WithExec([]string{"npm", "run", "test"})               // execute npm test command
+
+	// get test output
+	test, err := npm.Stdout(ctx)
+	if err != nil {
+		return err
+	}
+	// print output to console
+	fmt.Println(test)
+
+	// execute build command and get build output
+	build, err := npm.WithExec([]string{"npm", "run", "build"}).Stdout(ctx)
+	if err != nil {
+		return err
+	}
+	// print output to console
+	fmt.Println(build)
+
+	// all good
 	return nil
 }
