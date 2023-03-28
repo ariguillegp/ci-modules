48521b1f8d672c7803a0a4e219dc2c431f846d5e
Aristides Gonzalez <ariguille.gp@gmail.com>
2023-03-28 18:21:22 -0400

    var for base node image

--- js/build.go
+++ js/build.go
@@ -8,7 +8,7 @@ import (
 func (p *Pipeline) Build(ctx context.Context, baseNodeImg string) error {
 	src := p.Client.Host().Directory(".") // get the project source directory
 
-	npm := p.Client.Container().From("node"). // initialize new container from npm image
+	npm := p.Client.Container().From(baseNodeImg). // initialize new container from npm image
 							WithMountedDirectory("/src", src).WithWorkdir("/src"). // mount source directory to /src
 							WithExec([]string{"npm", "install"}).                  // execute npm install
 							WithExec([]string{"npm", "run", "test"})               // execute npm test command
